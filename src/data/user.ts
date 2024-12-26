8ab22499ebb84b26b807215c01de5b1a1c3b8c3d
Diogo Capdeville <diogoj.r.c@hotmail.com>
2024-12-25 21:42:26 -0300

    add `user` type and data

---
+++ src/data/user.ts
@@ -0,0 +1,12 @@
+import { User } from "@/types/user";
+
+export const user: User = {
+  slug: "diogojrc",
+  name: "Diogo Capdeville",
+  avatar: "https://ui-avatars.com/api/?background=random&name=Diogo&size=100",
+  cover:
+    "https://img.freepik.com/fotos-gratis/conceito-de-viagem-completo-com-pontos-de-referencia_23-2149153258.jpg",
+  bio: "Desenvolvedor fullstack há mais de 10 anos.",
+  link: "diogojrc.dev.br",
+  postCount: 18,
+};
